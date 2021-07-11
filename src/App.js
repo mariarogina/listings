6969a82e1886e40d1406d1fc33ad73f49965d899
Mariia <whitesheepa@gmail.com>
2021-07-12 01:04:45 +0300

    format

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
-import './App.css';
-import Listing from './Listing';
+import "./App.css";
+import Listing from "./Listing";
 
 function App() {
   return (
     <div>
-     <Listing/>
+      <Listing />
     </div>
   );
 }
